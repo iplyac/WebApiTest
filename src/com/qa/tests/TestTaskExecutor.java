e91c8f087c4c21c2e9ef3f9f2a359ca5ac586a50
iplyac <moss_trooper@mail.ru>
2015-12-21 03:13:09 +0500

    commit2

--- src/com/qa/tests/TestTaskExecutor.java
+++ src/com/qa/tests/TestTaskExecutor.java
@@ -16,7 +16,7 @@ import java.util.*;
 public class TestTaskExecutor {
 
     /**
-     * qqqq
+     * qqqqcfdfds
      *
      */
     private static Logger logger = Logger.getLogger(TestTaskExecutor.class);
